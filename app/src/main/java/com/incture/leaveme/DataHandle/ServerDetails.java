9b75140507e92f81f78baf9c64bf99fd33396e18
cseshaiban <cseshaiban@gmail.com>
2015-10-19 12:57:54 +0530

    Leave app V1.0

---
+++ app/src/main/java/com/incture/leaveme/DataHandle/ServerDetails.java
@@ -0,0 +1,15 @@
+package com.incture.leaveme.DataHandle;
+
+/**
+ * Created by Arjun on 13-10-2015.
+ */
+public class ServerDetails  {
+
+
+    public static final String RESPONSE_SUCCESS = "success";
+
+    public static final String UNIQUE_ID = "561ca12921d98b4b0e72b086";
+    public static final String APPLY_LEAVE = "http://inctureprod.cherrywork.in:8000/leave";
+    public static final String LEAVE_SUMMARY = "http://inctureprod.cherrywork.in:8000/leave-summary";
+
+}
